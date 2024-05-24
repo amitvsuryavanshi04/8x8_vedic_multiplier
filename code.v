4d4916b9fd12f2f7ed572657d5f00121bde20872
Sureshmadiwal <137718193+Sureshmadiwal@users.noreply.github.com>
2024-05-24 19:36:02 +0530

    Update code.v

--- code.v
+++ code.v
@@ -1,1 +1,8 @@
 //using the half adder code initially for 1st step calculation 
+
+module ha(a,b,sum,carry); //half adder module 
+input a,b;
+output sum,carry;
+xor(sum,a,b);
+and(carry,a,b);
+endmodule
